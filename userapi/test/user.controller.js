d0f74324e562130b18d182897cb780a82165b659
Gioia3022 <gioiagaliazzo04@gmail.com>
2022-12-19 14:43:24 +0100

    fix: userController when user not found

--- userapi/test/user.controller.js
+++ userapi/test/user.controller.js
@@ -44,11 +44,14 @@ describe('User', () => {
         firstname: 'Sergei',
         lastname: 'Kudinov'
       };
-      userController.create(user, (err, result) => {
-        expect(err).to.be.not.equal(null);
-        expect(result).to.be.equal(null);
-        done();
+      //Create a user
+      userController.create(user, () => {
+        userController.create(user, (err, result) => {
+          expect(err).to.be.not.equal(null);
+          expect(result).to.be.equal(null);
+          done();
       });
+    })
     });
   })
 
@@ -76,18 +79,11 @@ describe('User', () => {
     })
 
     // TODO Create test for the get method
-    it('cannot get a user when it does not exist', (done) => {
-      // 1. First, create a user to make this unit test independent from the others
-      // 2. Then, check if the result of the get method is correct
-      const user = {
-        username: 'sergkudinov',
-        firstname: 'Sergei',
-        lastname: 'Kudinov'
-      };
-      userController.get(user.username,(err,result) => {
-        expect(err).to.be.not.equal(null);
-        expect(result).to.be.equal(null);
-        done();
+    it('can not get a user when it does not exist', (done) => {
+      userController.get('invalid', (err, result) => {
+        expect(err).to.not.be.equal(null)
+        expect(result).to.be.equal(null)
+        done()
       });
     });
 
